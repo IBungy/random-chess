ff5f13be181dbe7f6d4d9f0aa1642d1901ad40fe
IBungy <bcain4902@gmail.com>
2025-11-10 14:25:32 -0500

    fixed some bugs

--- main.py
+++ main.py
@@ -1,20 +1,21 @@
 import chess
 import random
 
-def random_move(board, color=None):
+def random_move(board, color):
+    candidate = board.copy()
     if color == "white":
         turn_bool = chess.WHITE
     elif color == "black":
         turn_bool = chess.BLACK
     candidate.turn = turn_bool
     legal_moves = list(candidate.legal_moves)
-    if legal_moves not None:
+    if legal_moves:
         return random.choice(legal_moves)
     else:
         raise Exception("Draw or checkmate detected")
 
-def main(board):
-    move = random_move(board)
+def main(board, color):
+    move = random_move(board, color)
     return move
 
 
