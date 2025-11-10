b90efd221ddca6048f45e9e57ab00ee6b9904dc0
IBungy <bcain4902@gmail.com>
2025-11-10 14:36:27 -0500

    fixed sysarg

--- main.py
+++ main.py
@@ -1,27 +1,31 @@
 import chess
 import random
+import sys
 
 def random_move(board, color):
     candidate = board.copy()
-    if color == "white":
-        turn_bool = chess.WHITE
-    elif color == "black":
-        turn_bool = chess.BLACK
-    candidate.turn = turn_bool
+    candidate.turn = chess.WHITE if color == "white" else chess.BLACK
     legal_moves = list(candidate.legal_moves)
     if legal_moves:
         return random.choice(legal_moves)
     else:
         raise Exception("Draw or checkmate detected")
 
-def main(board, color):
+def main(board_fen, color):
+    board = chess.Board(board_fen)
     move = random_move(board, color)
     return move
 
-
 if __name__ == "__main__":
-    main()
+    if len(sys.argv) != 3:
+        print("Usage: python3 main.py '<fen_string>' <white|black>")
+        sys.exit(1)
 
+    fen = sys.argv[1]
+    color = sys.argv[2].lower()
+
+    move = main(fen, color)
+    print(move)
 
 
 
